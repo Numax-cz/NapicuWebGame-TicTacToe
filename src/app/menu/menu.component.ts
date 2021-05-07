253d950f46cce1e2bfa777532587e7ba5b35d30b
Marcel <marcel.mikolas@gmail.com>
2021-05-07 20:12:34 +0200

    App

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -32,7 +32,26 @@ export class MenuComponent implements OnInit {
       this.box.splice(pos, 1, this.Player);
       this.XNext = !this.XNext;
     }
+    this.winner = this.Winner();
   }
 
-
+  Winner() {
+    const line = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (var i = 0; i < line.length; i++){
+      const [a, b, c] = line[i];
+      if (this.box[a] === this.box[b] && this.box[a] === this.box[c]) {
+        return this.box[a]
+      }
+    }
+    return null;
+  }
 }
